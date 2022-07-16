7d68b1474126679e77b16ce479042c372e6081cd
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-16 15:15:33 +0800

    Add function that plays one round and returns the outcome

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     const choiceNum = Math.floor(Math.random() * 3);
     let choiceHand;
-    switch(choiceNum) {
+    switch (choiceNum) {
         case 0:
             choiceHand = "Rock";
             break;
@@ -15,4 +15,63 @@ function getComputerChoice() {
             console.log("Computer can't play");
     }
     return choiceHand;
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.slice(-1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    let playerOutcome;
+    switch (playerSelection) {
+        case "Rock":
+            switch (computerSelection) {
+                case "Rock":
+                    playerOutcome = "Draw";
+                    break;
+                case "Paper":
+                    playerOutcome = "Lose";
+                    break;
+                case "Scissor":
+                    playerOutcome = "Win";
+                    break;
+            }
+            break;
+
+        case "Paper":
+            switch (computerSelection) {
+                case "Rock":
+                    playerOutcome = "Win";
+                    break;
+                case "Paper":
+                    playerOutcome = "Draw";
+                    break;
+                case "Scissor":
+                    playerOutcome = "Lose";
+                    break;
+            }
+            break;
+            
+        case "Scissor":
+            switch (computerSelection) {
+                case "Rock":
+                    playerOutcome = "Lose";
+                    break;
+                case "Paper":
+                    playerOutcome = "Win";
+                    break;
+                case "Scissor":
+                    playerOutcome = "Draw";
+                    break;
+            }
+            break;
+    }
+
+    switch (playerOutcome) {
+        case "Win":
+            return `You ${playerOutcome}! ${playerSelection} beats ${computerSelection}`;
+        case "Draw":
+            return `The game is a ${playerOutcome}!`;
+        case "Lose":
+            return `You ${playerOutcome}! ${computerSelection} beats ${playerSelection}`;
+    }
+
 }
